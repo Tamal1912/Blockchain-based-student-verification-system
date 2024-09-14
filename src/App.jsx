1f73bf772357d7be23fe33c91c560afffa9b423e
Tamal Sur <tamalsur14@gmail.com>
2024-09-14 12:56:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl'>Hello Project</h1>
+      <h2 className='text-xl font-mono '>Blockchain based Student Verification System</h2>
+    </>
+  )
+}
+
+export default App
